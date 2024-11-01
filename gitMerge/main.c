e218d680469d129d6d49e8f4379bcd38845f3a24
Roger Fontoura <roger.fontoura@senairs.org.br>
2024-11-01 13:44:49 -0300

    Space

--- gitMerge/main.c
+++ gitMerge/main.c
@@ -7,7 +7,7 @@ void Drv_Test_function2(void);
 void App_Exec(void)
 {
     Drv_Test_function1();
-    printf("App Exec - main branch\n");
+    printf("App Exec - main branch 1\n");
     Drv_Test_function2();
 }
 
