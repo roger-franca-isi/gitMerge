df5f59487f5fc5405f0aea970b717bc37d47f320
Róger França <roger.fontoura@senairs.org.br>
2024-11-01 00:10:18 -0300

    Main Branch funcioando

---
+++ gitMerge/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void App_Exec(void);
+void Drv_Test_function1(void);
+void Drv_Test_function2(void);
+
+void App_Exec(void)
+{
+    Drv_Test_function1();
+    printf("App Exec - main branch\n");
+    Drv_Test_function2();
+}
+
+void Drv_Test_function1(void)
+{
+    printf("Drv Function 1 - main branch\n");
+}
+
+void Drv_Test_function2(void)
+{
+    printf("Drv Function 2 - main branch\n");
+}
+
+void main(void)
+{
+    App_Exec();
+}
